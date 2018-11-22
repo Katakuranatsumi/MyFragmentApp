6246d4c9085e81c47aaefcc08def8abe20a6eae0
Katakuranatsumi <usako7233@gmail.com>
2018-11-22 22:45:19 +0900

    2018/11/22　横画面実装

--- app/src/main/java/com/example/katakuranatsumi/myfragmentapp/TitlesFragment.java
+++ app/src/main/java/com/example/katakuranatsumi/myfragmentapp/TitlesFragment.java
@@ -1,13 +1,44 @@
 package com.example.katakuranatsumi.myfragmentapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TitlesFragment extends ListFragment {
 
+    public final static String EXTRA_POSITION =
+            "com.example.katakuranatsumi.myfragmentapp.POSITION";
+
+    private OnTitleSelectedListener listener;
+
     public TitlesFragment(){}
+
+    public interface OnTitleSelectedListener {
+      public void onTitleSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+          listener = (OnTitleSelectedListener) context;
+        } catch (ClassCastException e){
+            throw new ClassCastException(context.toString() +
+            " must inplement onTitleSelected");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
 
     @Override
     public void onActivityCreated( Bundle savedInstanceState) {
@@ -18,5 +49,13 @@ public class TitlesFragment extends ListFragment {
                 android.R.layout.simple_expandable_list_item_1,
                 News.Titles
         ));
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent intent = new Intent(getActivity(), SubActivity.class);
+        intent.putExtra(EXTRA_POSITION, position);
+        startActivity(intent);
     }
 }
