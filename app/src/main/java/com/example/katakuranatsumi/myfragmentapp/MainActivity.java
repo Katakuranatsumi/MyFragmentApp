61f00dbe6a8d6504d2937f3fe6c109e46589227c
Katakuranatsumi <usako7233@gmail.com>
2018-11-23 12:58:31 +0900

    2018/11/23 FragmentApp完成

--- app/src/main/java/com/example/katakuranatsumi/myfragmentapp/MainActivity.java
+++ app/src/main/java/com/example/katakuranatsumi/myfragmentapp/MainActivity.java
@@ -5,11 +5,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements TitlesFragment.OnTitleSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onTitleSelected(int position) {
+        DetailFragment detailFragment = DetailFragment.newInstance(position);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.detailFrame, detailFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
